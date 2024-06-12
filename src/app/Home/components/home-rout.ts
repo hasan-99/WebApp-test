c619487a5ed3b22a9b762266c384f89dac6646a4
Hasan Jahouse <hjahouse@techlink.com.cy>
2024-06-12 13:49:50 +0300

    small update in rout

--- src/app/Home/components/home-rout.ts
+++ src/app/Home/components/home-rout.ts
@@ -1,7 +1,7 @@
 import { Route } from '@angular/router';
 
 export default [
-          { path: '', redirectTo: '/test', pathMatch: 'full' },
+          { path: '', redirectTo: '/auth/login-page', pathMatch: 'full' },
 
           { path: 'login-page', loadComponent: () => import('./sign-in/sign-in.component').then((c) => c.SignInComponent) },
           { path: 'home', loadComponent: () => import('./home/home.component').then((c) => c.HomeComponent) },
